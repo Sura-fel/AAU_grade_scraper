f5cdd9658fbf12e7a601825708ef428785a88e7f
Surafel <fkresurafel@gmail.com>
2023-07-29 18:11:27 +0300

    Separated argparser file and scraper file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import argparse
-import requests_html as rh
-import pandas as pd
 
 parser = argparse.ArgumentParser(
     usage="%(prog)s -u 'Username' -P 'password' [-o file_name]",
@@ -33,23 +31,5 @@ parser.add_argument(
     )
 args = parser.parse_args()
 
-login_url = 'https://portal.aau.edu.et/login'
-home_url = 'https://portal.aau.edu.et/Home'
-grade_url = 'https://portal.aau.edu.et/Grade/GradeReport'
-
-
-session = rh.HTMLSession()
-login_response = session.get(login_url)
-hidden = login_response.html.find('form input[type="hidden"]',first=True)
-
-form = {hidden.attrs["name"]: hidden.attrs["value"]}
-form['UserName'] = args.username
-form['Password'] = args.password
-
-
-response = session.post(login_url,data=form)
-grade_html = session.get(grade_url).content
-
-df = pd.read_html(grade_html)[0]
-df.to_csv('{}.csv'.format(args.output))
-print('saved to {}.csv'.format(args.output))
+import scraper
+scraper.scrape(args)
